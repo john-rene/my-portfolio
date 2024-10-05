75600c8a576a9e1492e9fb9c73d2783804e736d8
john-rene <johnrenevalenzuela@protonmail.com>
2024-10-04 23:07:26 -0300

    Update introduction section and remove hand emoji

--- components/intro.tsx
+++ components/intro.tsx
@@ -40,20 +40,6 @@ export default function Intro() {
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
-
-          <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
-            initial={{ opacity: 0, scale: 0 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{
-              type: "spring",
-              stiffness: 125,
-              delay: 0.1,
-              duration: 0.7,
-            }}
-          >
-            👋
-          </motion.span>
         </div>
       </div>
 
@@ -62,11 +48,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm John.</span> I'm a tech aficionado, and a{" "}
+        <span className="font-bold">Java developer. I have been programming </span>{" "}
+        <span className="font-bold">since I was 17. </span> I enjoy
+        building <span className="italic">sites & apps</span>. My focus is in{" "}
+        <span className="underline">React (Next.js), Java and Python</span>.
       </motion.h1>
 
       <motion.div
@@ -94,7 +80,7 @@ export default function Intro() {
           href="/CV.pdf"
           download
         >
-          Download CV{" "}
+          Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
 
