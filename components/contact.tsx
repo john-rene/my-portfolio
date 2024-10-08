c136bd30fdd02f7b3527855e2b2bc88a17865e35
john-rene <johnrenevalenzuela@protonmail.com>
2024-10-08 12:25:20 -0300

    update email look

--- components/contact.tsx
+++ components/contact.tsx
@@ -34,7 +34,7 @@ export default function Contact() {
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:contact@johnrene.net">
-            Contact@JohnRene.net
+            contact@johnrene.net
         </a>{" "}
         or through this form.
       </p>
